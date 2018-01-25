2810aaf42016a660456262f4b4be7f6ecab70d29
Marko Röder <marko.roeder@engagelively.com>
2018-01-25 10:13:10 -0800

    Add timestamps to auth server log (copied from main server)

--- bin/start-server.js
+++ bin/start-server.js
@@ -10,6 +10,8 @@ var parseArgs = require('minimist'),
     defaultRootDirectory = process.cwd(),
     isMain = !module.parent,
     baseURL = "file://" + path.resolve(path.join(__dirname, ".."));
+const util = require('util');
+const winston = require('winston');
 
 if (isMain) {
   var args = parseArgs(process.argv.slice(2));
@@ -29,6 +31,8 @@ if (isMain) {
 
 
 function setupSystem() {
+  setupLogger();
+
   var livelySystem = lively.modules.getSystem("lively-for-auth-server", {baseURL});
   lively.modules.changeSystem(livelySystem, true);
   var registry = livelySystem["__lively.modules__packageRegistry"] = new lvm.PackageRegistry(livelySystem);
@@ -39,4 +43,20 @@ function setupSystem() {
   if (process.env.FLATN_DEV_PACKAGE_DIRS)
     registry.devPackageDirs = process.env.FLATN_DEV_PACKAGE_DIRS.split(":").map(ea => lively.resources.resource(`file://${ea}`));
   return registry.update();
+}
+
+function formatArgs(args){
+  return [util.format.apply(util.format, Array.prototype.slice.call(args))];
+}
+
+function setupLogger() {
+  let logger = new winston.Logger();
+  logger.add(winston.transports.Console, {colorize: true, timestamp: true});
+  console.livelyLogger = logger;
+  console.log = function() { logger.info.apply(logger, formatArgs(arguments)); };
+  console.info = function() { logger.info.apply(logger, formatArgs(arguments)); };
+  console.warn = function() { logger.warn.apply(logger, formatArgs(arguments)); };
+  console.error = function() { logger.error.apply(logger, formatArgs(arguments)); };
+  console.debug = function() { logger.debug.apply(logger, formatArgs(arguments)); };
+  return logger;
 }
